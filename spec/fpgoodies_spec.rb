3af08a67d64df879e4da0d1ad0a86e3783410dc1
Striking <Strikingwolf2012@gmail.com>
2015-10-05 17:04:39 -0500

    Spec for unions

--- spec/fpgoodies_spec.rb
+++ spec/fpgoodies_spec.rb
@@ -6,29 +6,42 @@ using ComposeEverything::Proc
 
 describe ComposeEverything::FunctionalGoodies do
   context "When implemented on a custom class" do
+    a = LinkedList.from_arr([1, 2, 3])
+    b = LinkedList.from_arr(["", "hi", "hello", "meow"])
+    hashmap = {:hello => 4, :meow => 5}
+
     it "Implements compose" do
-      a = LinkedList.from_arr([1, 2, 3])
-      b = LinkedList.from_arr(["", "hi", "hello", "meow"])
       c = a.compose(b)
       expect(c[0]).to eq "hi"
       expect(c[1]).to eq "hello"
       expect(c[2]).to eq "meow"
     end
+
+    it "Unions" do
+      c = a.union hashmap
+      expect(c[0]).to eq 1
+      expect(c[1]).to eq 2
+      expect(c[2]).to eq 3
+      expect(c[:hello]).to eq 4
+      expect(c[:meow]).to eq 5
+      expect(c[90]).to eq nil
+    end
   end
 
   context "Refinements" do
+    a = LinkedList.from_arr(["hi", "hello", "meow"])
+    b = ->(x) {
+      if x == "hi"
+        1
+      elsif x == "hello"
+        2
+      else
+        3
+      end
+    }
+    c = {1 => "life", 2 => "world", 3 => "striking"}
+
     it "Composes" do
-        a = LinkedList.from_arr(["hi", "hello", "meow"])
-        b = ->(x) {
-          if x == "hi"
-            1
-          elsif x == "hello"
-            2
-          else
-            3
-          end
-        }
-        c = {1 => "life", 2 => "world", 3 => "striking"}
         d = a.compose(b).compose(c)
         expect(d[0]).to eq "life"
         expect(d[1]).to eq "world"
